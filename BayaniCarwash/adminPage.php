3fad3cd27510d43faf3f699c42e89d5a37ba5fc5
n8tan <nathanrenante@gmail.com>
2016-02-20 01:39:59 +0800

    Update: -Created an admin page. -Added additional background images to the images folder.
    Notes:
    -The design for the admin page is just a rough design. Please add additional styling.

---
+++ BayaniCarwash/adminPage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Admin Page</title>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link href="styles.css" rel="stylesheet" />
+ <style>
+  body {
+ 	 background-image: url("images/carbon_fibre_big.png");
+  }
+  h1.title{
+     text-align: center;
+     font-family: serif;
+    
+
+  }
+  .addCar{
+  	background-image: url("images/polonez_car.png")
+  }
+  
+
+  </style>
+
+
+</head>
+    <body>
+    <div class="container">
+    <div class="jumbotron addCar">
+    	<h1 class="title"><font color="black">Admin Page</font></h1>
+    	
+    </div>
+    
+    	 <button type="button" class="btn btn-primary btn-block addCar hvr-shrink" >
+    	 <font color="black" size="30">Insert Text Here</font></button>
+    	 <br><br>
+    	 
+    	 <button type="button" class="btn btn-primary btn-block addCar hvr-shrink">
+    	 <font color="black" size="30">Insert Text Here</font></button>
+    	 <br><br>
+    	 
+    	 <button type="button" class="btn btn-primary btn-block addCar hvr-shrink">
+    	 <font color="black" size="30">Insert Text Here</font></button>
+    	 <br><br>
+    	 
+    	 <button type="button" class="btn btn-primary btn-block addCar hvr-shrink">
+    	 <font color="black" size="30">Insert Text Here</font></button>
+    
+    </div>
+    
+    <?php
+	?>
+    </body>
+</html>
